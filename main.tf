b65a1ade5bf3bcf28a1c0a71f5b61ec2949c1f52
darreng <darreng@spaceflightindustries.com>
2017-01-04 15:57:08 -0800

    can't believe I missed that

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ variable "subnet_pub_ip" { default = false }
 variable "subnet_az" { }
 
 resource "aws_subnet" "ec2_subnet" {
-  /*vpc_id                  = "${subnet_vpc_id}"*/
-  /*availability_zone       = "${subnet_az}"*/
-  cidr_block              = "${subnet_cidr_block}"
-  /*map_public_ip_on_launch = "${subnet_pub_ip}"*/
+  vpc_id                  = "${var.subnet_vpc_id}"
+  availability_zone       = "${var.subnet_az}"
+  cidr_block              = "${var.subnet_cidr_block}"
+  map_public_ip_on_launch = "${var.subnet_pub_ip}"
 }
