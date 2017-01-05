ee49d1fee3a4d9d920c65a9af0368bc27af3457c
darreng <darreng@spaceflightindustries.com>
2017-01-05 09:25:03 -0800

    Adding in a subnet tag var.

--- main.tf
+++ main.tf
@@ -2,10 +2,15 @@ variable "subnet_vpc_id" { }
 variable "subnet_cidr_block" { }
 variable "subnet_pub_ip" { default = false }
 variable "subnet_az" { }
+varialbe "subnet_tag_name" { }
 
 resource "aws_subnet" "ec2_subnet" {
   vpc_id                  = "${var.subnet_vpc_id}"
   availability_zone       = "${var.subnet_az}"
   cidr_block              = "${var.subnet_cidr_block}"
   map_public_ip_on_launch = "${var.subnet_pub_ip}"
+
+  tags {
+    Name = "${var.subnet_tag_name}"
+  }
 }
